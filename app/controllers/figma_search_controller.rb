27faee8a0143c26761a1566c617dae97f96818fe
Theo Luciano <62947477+theoluciano@users.noreply.github.com>
2025-04-22 07:39:09 -0400

    add searching indicator

--- app/controllers/figma_search_controller.rb
+++ app/controllers/figma_search_controller.rb
@@ -1,9 +1,10 @@
 class FigmaSearchController < ApplicationController
   def index
     @projects = FigmaScanner.fetch_projects_for_known_teams || []
-    @results = []
-
-    if params[:component].present? && params[:project_id].present?
+    @results = nil
+    @searching = params[:component].present? && params[:project_id].present?
+  
+    if @searching
       scanner = FigmaScanner.new(params[:project_id], params[:component])
       @results = scanner.find_component_matches
     end
